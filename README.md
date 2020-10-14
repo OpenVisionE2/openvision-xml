0a19ae4a19ac22be1af9b4811e04cddcc3dbfdc7
persianpros <persianpros@gmail.com>
2020-10-15 02:06:26 +0330

    Import

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# openvision-xml
+openvision-xml [![Build Status](https://travis-ci.org/OpenVisionE2/openvision-xml.svg?branch=master)](https://travis-ci.org/OpenVisionE2/openvision-xml) [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+==============
 Open Vision xml files
---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+get_files() {
+  cd data
+  wget -O iso-639-3.tab http://www-01.sil.org/iso639-3/iso-639-3.tab
+  python convert-iso-639-3.py
+  cd ..
+  cd xml
+  wget -O atsc.xml  https://raw.githubusercontent.com/oe-alliance/oe-alliance-tuxbox-common/master/src/atsc.xml
+  wget -O cables.xml  https://raw.githubusercontent.com/oe-alliance/oe-alliance-tuxbox-common/master/src/cables.xml
+  wget -O terrestrial.xml  https://raw.githubusercontent.com/oe-alliance/oe-alliance-tuxbox-common/master/src/terrestrial.xml
+}
+
+setup_git() {
+  git config --global user.email "bot@openvision.tech"
+  git config --global user.name "Open Vision bot"
+}
+
+commit_files() {
+  git checkout master
+  git add -u
+  git add *
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add upstream https://${GH_TOKEN}@github.com/OpenVisionE2/openvision-xml.git > /dev/null 2>&1
+  git push --quiet --set-upstream upstream master || echo "failed to push with error $?"
+}
+
+setup_git
+get_files
+commit_files
+upload_files
