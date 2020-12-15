fc08edb89af4a047cd2e98b213bad4456083f724
Persian Prince <persianpros@yahoo.com>
2020-12-15 23:46:06 +0330

    timezone.xml fetch support

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ get_files() {
   wget -O atsc.xml https://raw.githubusercontent.com/oe-alliance/oe-alliance-tuxbox-common/master/src/atsc.xml
   wget -O cables.xml https://raw.githubusercontent.com/oe-alliance/oe-alliance-tuxbox-common/master/src/cables.xml
   wget -O terrestrial.xml https://raw.githubusercontent.com/oe-alliance/oe-alliance-tuxbox-common/master/src/terrestrial.xml
+  wget -O timezone.xml https://raw.githubusercontent.com/oe-alliance/oe-alliance-tuxbox-common/master/src/timezone.xml
   cd ..
 }
 
