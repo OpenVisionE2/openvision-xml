af09c9466b895b9e5b536a2a0b32244ef59d9e7b
persianpros <persianpros@yahoo.com>
2022-08-16 11:34:37 +0430

    Create iso-639-3.pck for both py versions

--- CI/iso-639-3.sh
+++ CI/iso-639-3.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 cd data
+rm -f iso-639-3.pck
 wget -O iso-639-3.tab http://www-01.sil.org/iso639-3/iso-639-3.tab
-python convert-iso-639-3.py
+python2 convert-iso-639-3.py
+mv -f iso-639-3.pck py2
+sleep 0.1
+python3 convert-iso-639-3.py
+mv -f iso-639-3.pck py3
 sleep 1
 cd ..
 
